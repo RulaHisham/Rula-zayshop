23848b589e2ed7d885ab3de79589d2cdad84cfc7
RulaHisham <duraidirula@gmail.com>
2023-12-02 21:20:11 +0200

    first commit

---
+++ resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Zay Shop eCommerce HTML CSS Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/templatemo.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/custom.css')}}">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/fontawesome.min.css')}}">
+    <!--
+
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+-->
+</head>
+
+<body>
+    <!-- Start Top Nav -->
+   @include('frontend.layout.partials.nav')
+    <!-- Close Top Nav -->
+
+
+    <!-- Header -->
+   @include('frontend.layout.partials.header')
+    <!-- Close Header -->
+
+    <!-- Modal -->
+  @include('frontend.layout.partials.modle')
+    <section class="content">
+
+    <div class="container-fluid">
+    @yield('content')
+    </div>
+
+
+
+    <!-- Start Footer -->
+    @include('frontend.layout.partials.footer')
+<!-- Start javascript -->
+
+
+    @include('frontend.layout.partials.script')
+
---
+++ resources/views/frontend/layout/partials/script.blade.php
@@ -0,0 +1,7 @@
+ <!-- Start Script -->
+    <script src="{{asset('frontend/assets/js/jquery-1.11.0.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/jquery-migrate-1.2.1.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/templatemo.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/custom.js')}}"></script>
+    <!-- End Script -->
